53d3a94cbb848c5ad9b8be95e8ef1271c058c9c9
Antony Kyalo Musyoki <antony.kyalo@bookinghub.com>
2025-12-30 22:08:23 -0600

    Module-04-03 END

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,18 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
     selector: 'saa-app',
     templateUrl: './app.component.html',
-    standalone: false
+    encapsulation: ViewEncapsulation.None,
+    standalone: false,
+    styleUrls: [
+        './shared/scss/root/normalize.scss',
+        './shared/scss/root/root.scss',
+        './shared/scss/root/links.scss',
+        './shared/scss/layout/app-footer.scss',
+        './shared/scss/layout/app-header.scss',
+        './shared/scss/layout/layout.scss',
+        './shared/scss/typography/headers.scss',       
+        './shared/scss/typography/paragraphs.scss',
+    ]
 })
 
 export class AppComponent {
