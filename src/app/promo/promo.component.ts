61fbb5452db332566d88f72b577edbd881a2dc2a
Antony Kyalo Musyoki <antony.kyalo@bookinghub.com>
2026-01-15 00:55:39 -0600

    ch05/Adding Classes and using :host

--- src/app/promo/promo.component.ts
+++ src/app/promo/promo.component.ts
@@ -8,6 +8,13 @@ import { Component, ElementRef, inject } from '@angular/core';
 })
 
 export class PromoComponent {
+    //Get Reference to host element so we can read its class list
+    //use inject() function to inject ElementRef class. 
+    //This gives us access the host element 
     private hostRef = inject(ElementRef);
+    //isColor01 is a boolean property set based on whether the 
+    //host element has teh color-01 class on it.
+    //Use nativeElement to access the host HTML element then see if
+    //its class list contains the color--01 class
     protected isColor01 = this.hostRef.nativeElement.classList.contains('color--01');
 }
